cc8739f6375c24400b6fea7e27da7b045171cf16
ryan131 <randyryan@gmail.com>
2023-06-12 16:05:09 +0800

    [Carbon Angular V10] Data time picker layout of different sizes

--- carbon-angular-v10/src/app/components/data-time/data-time.module.ts
+++ carbon-angular-v10/src/app/components/data-time/data-time.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonModule, DropdownModule } from 'carbon-components-angular';
-import { ChevronDownModule, PauseModule, PlayModule, TimeModule, WarningAltFilledModule, WarningFilledModule } from '@carbon/icons-angular';
+import { ButtonModule, DropdownModule, LoadingModule } from 'carbon-components-angular';
+import {
+  ChevronDownModule,
+  PauseModule,
+  PlayModule,
+  TimeModule,
+  WarningAltFilledModule,
+  WarningFilledModule
+} from '@carbon/icons-angular';
 
 import { DataTimePickerComponent } from './data-time-picker/data-time-picker.component';
 import { DataTimeSelectComponent } from './data-time-select/data-time-select.component';
@@ -13,14 +20,15 @@ import { DataTimeSelectComponent } from './data-time-select/data-time-select.com
   ],
   imports: [
     CommonModule,
+    ButtonModule,
+    DropdownModule,
+    LoadingModule,
     ChevronDownModule,
-    WarningAltFilledModule,
-    WarningFilledModule,
-    TimeModule,
     PauseModule,
     PlayModule,
-    ButtonModule,
-    DropdownModule
+    TimeModule,
+    WarningAltFilledModule,
+    WarningFilledModule,
   ],
   exports: [
     DataTimePickerComponent,
