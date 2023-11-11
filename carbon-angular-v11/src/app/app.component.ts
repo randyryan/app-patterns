1f1d6f04ad745d5ae144db8d1047ad2dce11744f
ryan131 <randyryan@gmail.com>
2023-11-11 21:22:45 +0800

    [Carbon Angular V11] Theme

--- carbon-angular-v11/src/app/app.component.ts
+++ carbon-angular-v11/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, Renderer2, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  document = inject(DOCUMENT);
+  renderer = inject(Renderer2);
+  theme = "cds--theme--g10";
 
   sidenavExpanded = false; // Sidenav collapsed by default
 
   constructor() {
+  }
+
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, this.theme);
   }
 
   toggleSidenav(expanded: boolean): void {
